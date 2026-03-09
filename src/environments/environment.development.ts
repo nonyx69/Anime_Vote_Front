33304bb53d4ee008a814ff05e650131f245e0be7
nonyx_69 <noa.guilhot.pro@gmail.com>
2026-03-10 00:25:42 +0100

    Initialisation du projet Angular

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+// Pour le dev :
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+// Pour la prod :
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:8000/',
+};
